87bab06e663b3b0b1d76635602ec8496acba463d
dbagnyuk <dbagnyuk@gmail.com>
2019-11-23 13:01:10 +0100

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+
+namespace CopyWorkItemFromTFStoHTMLandAttachWin
+{
+    public abstract class Config
+    {
+        /// <summary>
+        /// Create/Edit/Read the Config File.
+        /// </summary>
+
+        // string array for read the config file
+        public static string[] config = null;
+        // temp variable for read value from Config Form
+        public static string tempDomainName = null;
+        public static string tempPassword = null;
+        public static string tempPathToTasks = null;
+
+        // read the config file to memory
+        public static bool readConfigFile(string configFile, ref string[] config)
+        {
+            // check if the config file exist and if it more than 0 bytes
+            if (!File.Exists(configFile) || new FileInfo(configFile).Length == 0)
+                return false;
+
+            // catch error of reading the config file
+            try
+            {
+                // read config file into string array
+                config = System.IO.File.ReadAllLines(configFile);
+            }
+            catch (Exception ex)
+            {
+                Program.exExit(ex);
+            }
+            // chek config if it has less or more than 3 string
+            if (config.Length != 3)
+                return false;
+
+            // check the string from string array if it's empty or not
+            foreach (var s in config)
+                if (s.Length == 0)
+                    return false;
+
+            return true;
+        }
+        // function for create and editing the config file
+        public static void editConfigFile(string ConfFile)
+        {
+            string[] tempConfig = null;
+
+            FileStream fileStream = null;
+            StreamWriter streamWriter = null;
+
+            // check if config exists or biger than 0 byes
+            if (File.Exists(ConfFile) && new FileInfo(ConfFile).Length != 0)
+            {
+                try
+                {
+                    tempConfig = System.IO.File.ReadAllLines(ConfFile);
+                    fileStream = new FileStream(ConfFile, FileMode.Truncate);
+                }
+                catch (Exception ex)
+                {
+                    Program.exExit(ex);
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.Delete(ConfFile);
+                    fileStream = new FileStream(ConfFile, FileMode.CreateNew);
+                }
+                catch (Exception ex)
+                {
+                    Program.exExit(ex);
+                }
+            }
+            streamWriter = new StreamWriter(fileStream);
+
+            if (tempDomainName.CompareTo("") == 0)
+                streamWriter.WriteLine(tempConfig[0].ToLower());
+            else
+                streamWriter.WriteLine(tempDomainName.ToLower());
+
+            // encrypt the password for writing into config file
+            streamWriter.WriteLine(Cipher.Encrypt(tempPassword, Program.Key));
+
+            if (tempPathToTasks.CompareTo("") == 0)
+                streamWriter.WriteLine(tempConfig[2].ToLower());
+            else
+                streamWriter.WriteLine(tempPathToTasks.ToLower());
+
+            streamWriter.Close();
+            fileStream.Close();
+        }
+    }
+}
---
+++ mainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CopyWorkItemFromTFStoHTMLandAttachWin
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            if (!Config.readConfigFile(Program.configFile, ref Config.config))
+                configToolStripMenuItem_Click(sender, e);
+
+            // Creating and setting the 
+            // properties of the ToolTip 
+            ToolTip t_Tip = new ToolTip();
+            t_Tip.Active = true;
+            t_Tip.AutoPopDelay = 4000;
+            t_Tip.InitialDelay = 500;
+            t_Tip.IsBalloon = true;
+            t_Tip.ToolTipIcon = ToolTipIcon.Info;
+            t_Tip.SetToolTip(textBoxTFSid, "TFS id contain only digits!");
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBoxTFSid.Text, out Program.itemId))
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                this.Enabled = false;
+                TFStoHTML.connectToTFS();
+                TFStoHTML.writeTFStoHTML();
+                Cursor.Current = Cursors.Default;
+                this.Enabled = true;
+            }
+            else
+            {
+                textBoxTFSid.Clear();
+                textBoxTFSid.Text = "Wrong input!";
+            }
+        }
+
+        private void toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBoxTFSid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+        private void textBoxTFSid_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                buttonFind_Click((object)sender, (EventArgs)e);
+            if (e.KeyCode == Keys.Escape)
+                textBoxTFSid.Clear();
+        }
+
+        private void aboutStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+             MessageBox.Show("Application for remote saving TFS Item information into HTML file,\n"
+                          + "and download Attachments from the TFS Item into the selected folder.\n\n"
+                          + "by Dim@", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBoxTFSid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTFSid_Click(object sender, EventArgs e)
+        {
+            textBoxTFSid.Clear();
+        }
+
+        private void checkBoxAttach_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxAttach.Checked)
+                Program.downConfirm = true;
+            else
+                Program.downConfirm = false;
+        }
+
+        private void configToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the Config Form class
+            configForm confForm = new configForm(this);
+
+            //foreach (Control item in this.Controls)
+            //    item.Enabled = false;
+
+            // Disable the Main Form and Show the Config Form
+            this.Enabled = false;
+            confForm.Show();
+            confForm.Activate();
+        }
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                DialogResult result = MessageBox.Show("Do you really want to exit?", "Close", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
